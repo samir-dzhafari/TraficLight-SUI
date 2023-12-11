f68138cf4feeae220e701c90b7606948f499c3a1
Samir Dzhafari <samirjafari2001@icloud.com>
2023-12-11 20:06:12 +0300

    Initial Commit

---
+++ TraficLight-SUI/TraficLight_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLight_SUIApp.swift
+//  TraficLight-SUI
+//
+//  Created by Самир Джафари on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TraficLight_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
